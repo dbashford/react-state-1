1b1fac4cb8e5397362d3a4acdb1cef21cb135fcd
David Bashford <david.bashford@emc.com>
2016-07-22 09:01:56 -0400

    comments

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Demo app for NOVAReact talk given on 7/21/16.
 
 ## Running
 `npm run dev`
+
+## Toggle between two examples
+
+In `entry.js` uncomment/comment the lines for the example app you wish to view.
+
+
--- src/client/entry.js
+++ src/client/entry.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// toggle between example 1 and 2 here
 import App from './components/example1/App';
+// import App from './components/example2/App';
 
 ReactDOM.render(
   <App />,
